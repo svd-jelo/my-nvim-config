67a03276b8f7a95130e61175adbbee295b33de12
svd-jelo <skulpayls21@gmail.com>
2026-05-29 11:26:02 +0800

    Added LSP support via mason.nvim, mason-lspconfig, and nvim-lspconfig

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = { "lua_ls", "pyright" },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("pyright")
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
