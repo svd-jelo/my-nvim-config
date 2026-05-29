8c1afc931f4278d84823aac9e589448884e85549
svd-jelo <skulpayls21@gmail.com>
2026-05-29 11:22:34 +0800

    Enable highlight and indent

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -5,7 +5,9 @@ return {
   config = function()
     require('nvim-treesitter').setup {
       -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-      install_dir = vim.fn.stdpath('data') .. '/site'
+      install_dir = vim.fn.stdpath('data') .. '/site',
+      highlight = { enable = true },
+      indent = { enable = true },
     }
     require('nvim-treesitter').install({ 'python', 'markdown', 'latex', 'lua' }):wait(300000) -- wait max. 5 minutes
     vim.api.nvim_create_autocmd('FileType', {
